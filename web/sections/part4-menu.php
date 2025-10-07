1b98579fb52d26c6f5a85b25a99ab7cc4a1b6a79
Matamaya <110430576+Matamaya@users.noreply.github.com>
2025-10-07 23:34:27 +0200

    Parte 4: Menú de platillos favoritos y sus precios

---
+++ web/sections/part4-menu.php
@@ -0,0 +1,37 @@
+<?php
+$dish = [
+    [
+        "title" => "Paella Valenciana", 
+        "price" => 12.0
+    ],
+    [
+        "title" => "Tacos al Pastor", 
+        "price" => 9.5
+    ],
+    [
+        "title" => "Pizza Napolitana", 
+        "price" => 15.0
+    ]
+];
+?>
+    <div class="container">
+        <h1>🍴 Foodies de la Empresa</h1>
+    </div>
+
+     <table>
+                <thead>
+                    <tr>
+                        <th>🥘 Platillo</th>
+                        <th>Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($dish as $dish): ?>
+                         <?php $formattedPrice = number_format($dish["price"], 2, ',', '.'); ?>
+                        <tr>
+                            <td><?php echo $dish["title"]; ?></td>
+                            <td><?php echo $formattedPrice; ?> €</td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+      </table>
