f9212aaa20b568485ec49d83f3bf1b4ddabc7818
Matamaya <110430576+Matamaya@users.noreply.github.com>
2025-10-07 23:48:45 +0200

    Parte 6: Viaje, especificando el destino y los días

---
+++ web/sections/part6-event.php
@@ -0,0 +1,13 @@
+<?php
+$destino = "Roma";
+$dias = 5;
+?>
+    <section>
+        <h1>
+            Mi próximo viaje
+        </h1>
+    </section>
+    <div class="tarjeta-viaje">
+        <p>Mi próximo viaje será a <?php echo $destino; ?>, durante <?php echo $dias; ?> días ✈️</p>
+    </div>
+</main>
