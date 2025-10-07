c9f8382eaf0e271be4418e5068cbc621c67b3b67
Matamaya <110430576+Matamaya@users.noreply.github.com>
2025-10-07 23:22:45 +0200

    Creación del Index.php usando las funciones de PHP include y require para añadir los componentes

---
+++ web/index.php
@@ -0,0 +1,24 @@
+<?php require __DIR__ . '/includes/header.php'; ?>
+
+<section id="paneles" class="container">
+    <section id="panel-perfil" class="tab-panel active" role="tabpanel">
+        <?php include __DIR__ . '/sections/part1-profile.php'; ?>
+    </section>
+    <section id="panel-musica" class="tab-panel active" role="tabpanel">
+        <?php include __DIR__ . '/sections/part2-music.php'; ?>
+    </section>
+    <section id="panel-movies" class="tab-panel active" role="tabpanel">
+        <?php include __DIR__ . '/sections/part3-movies.php'; ?>
+    </section>
+    <section id="panel-menu" class="tab-panel active" role="tabpanel">
+        <?php include __DIR__ . '/sections/part4-menu.php'; ?>
+    </section>
+    <section id="panel-event" class="tab-panel active" role="tabpanel">
+        <?php include __DIR__ . '/sections/part5-event.php'; ?>
+    </section>
+    <section id="panel-travel" class="tab-panel active" role="tabpanel">
+        <?php include __DIR__ . '/sections/part6-travel.php'; ?>
+    </section>
+</section>
+
+<?php require 'includes/footer.php'; ?>
